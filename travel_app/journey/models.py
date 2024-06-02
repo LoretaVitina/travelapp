09116c2a3f4267155f3abc222d33709331de0e02
LoretaVitina <56500810+LoretaVitina@users.noreply.github.com>
2024-06-02 12:00:30 +0300

    models ceated

--- travel_app/journey/models.py
+++ travel_app/journey/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    people_count = models.IntegerField(default=1)
+    title = models.CharField(max_length=200)
+
+class Event(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    comment = models.TextField()
+    address = models.CharField(max_length=200)
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
